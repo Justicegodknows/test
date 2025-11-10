9d44469c991dfd37e0408fba22c498efbcb29646
Justicegodknows <justice.samuel@code.berlin>
2025-11-10 19:10:25 +0100

    CHANGES 2

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr;
+use App\Models\Job;
 
-class Job {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$100,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Manager',
-                'salary' => '$80,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Software Engineer',
-                'salary' => '$60,000',
-            ],
-        ];
-    }
-}
+
+
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => $jobs
+        'jobs' => Job::all()
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs) {  
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+   
     return view('job', ['job' => $job]);
 });
     
