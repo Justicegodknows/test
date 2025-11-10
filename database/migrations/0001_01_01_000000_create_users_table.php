59cd0f3e6ea2af9b2274e50b93d110dcaf745f04
Justicegodknows <justice.samuel@code.berlin>
2025-11-10 22:19:15 +0100

    CHANGES 4

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,6 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    protected $table = 'users';
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
